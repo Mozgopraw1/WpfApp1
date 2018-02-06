d11e46360d76ca91b93c4a1b30b28d2a73f0972d
Mozgopraw1 <mr.devgen@gmail.com>
2018-02-06 23:02:27 +0500

    002
    Остановился на баге с num,  не получается вытащить ноль из строки, срабатывает исключение, строка 66.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,8 +22,9 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        int num, data;
-        char[] eng, rus;
+        int num, index, index2;
+        DateTime data1, data2;
+        string eng, rus, stroka, vrem;
         Boolean fgot = false, fgot1 = false, den1=false, den3=false, den7=false, den21=false, den50=false;
 
         public MainWindow()
@@ -39,11 +41,39 @@ namespace WpfApp1
 
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
-            FileStream file1 = new FileStream("test.txt", FileMode.Open);
-            StreamReader reader = new StreamReader(file1, Encoding.Unicode);
-            StreamWriter writer = new StreamWriter(file1, Encoding.UTF8);
-            writer.WriteLine(TextBox1.Text);
-            writer.Close();
+            using (StreamReader sw = new StreamReader("test.txt"))
+            {
+                String line;
+                while ((line = sw.ReadLine()) != null)
+                {
+                    num++;
+                }
+            }
+            eng = TextBox1.Text;
+            rus = TextBox2.Text;
+            data1 = DateTime.Today;
+            stroka = num.ToString() + '|' + eng + '|' + rus + '|' + data1.ToShortDateString() + '|' + fgot + '|' 
+                + fgot1 + '|' + den1 + '|' + den3 + '|' + den7 + '|' + den21 + '|' + den50 + '|';
+            using (StreamWriter sw = new StreamWriter(new FileStream("test.txt", FileMode.Append, FileAccess.Write)))
+            {
+                sw.WriteLine(stroka);
+                sw.Close();
+            }
+            using (StreamReader sw = new StreamReader("test.txt"))
+            {
+                stroka = sw.ReadLine();
+                index = stroka.IndexOf('|');
+                num = Convert.ToInt32(stroka.Substring(0, index-1));
+                TextBlock1.Text = num.ToString();
+                stroka = stroka.Remove(0, index+1);
+                index = stroka.IndexOf('|');
+                eng = stroka.Substring(0, index);
+                TextBlock1.Text = eng;
+                sw.Close();
+
+            }
+                /* Сброс */
+                num = 0;
         }
     }
 }
